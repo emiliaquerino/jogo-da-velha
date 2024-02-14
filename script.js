203e2a0217a4e34e0cefc2e237da981e00599b54
Emília Querino <emiliaquerino@gmail.com>
2024-02-14 16:56:16 -0300

    Add ação de validação da entrada do usuário

--- script.js
+++ script.js
@@ -19,16 +19,24 @@ window.addEventListener("DOMContentLoaded", () => {
     [2, 4, 6],
   ];
 
-  let board = ["", "", "", "", "", "", "", "", ""];
-  let currentPlayer = "X";
-  let isGameActive = true;
-
-  /*
+    /*
    Indexes within the board
    [0] [1] [2]
    [3] [4] [5]
    [6] [7] [8]
 */
 
+
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let currentPlayer = "X";
+  let isGameActive = true;
+
+const isValidAction = (box) => {
+    if (box.innerText === 'X' || box.innerText === 'O'){
+        return false;
+    }
+
+    return true;
+};
  
 });
