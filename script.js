1028c09c5c27c1fade38c2bfabfddfa9bfb6dd20
Emília Querino <emiliaquerino@gmail.com>
2024-02-14 16:42:07 -0300

    Add as variáveis globais de controle do jogo

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-
-  const boxes = Array.from(document.querySelectorAll(".box"));
+  const boxes = Array.from(document.querySelectorAll(".box")); //  retorna um NodeList, então temos que convertê-lo em um array usando Array.from()
   const playerDisplay = document.querySelector(".display-player");
   const resetButton = document.querySelector("#reset");
   const announcer = document.querySelector(".announcer");
 
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let currentPlayer = "X";
+  let isGameActive = true;
+
+  const PLAYERX_WON = "PLAYERX_WON";
+  const PLAYERO_WON = "PLAYERO_WON";
+  const TIE = "TIE";
 });
