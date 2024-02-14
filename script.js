d7d2e2f86c03e28561323a421cf6ebc287a74e68
Emília Querino <emiliaquerino@gmail.com>
2024-02-14 16:51:25 -0300

    Add as constantes com as posições vencedoras

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ window.addEventListener("DOMContentLoaded", () => {
   const resetButton = document.querySelector("#reset");
   const announcer = document.querySelector(".announcer");
 
+  const PLAYERX_WON = "PLAYERX_WON";
+  const PLAYERO_WON = "PLAYERO_WON";
+  const TIE = "TIE";
+
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   let board = ["", "", "", "", "", "", "", "", ""];
   let currentPlayer = "X";
   let isGameActive = true;
 
-  const PLAYERX_WON = "PLAYERX_WON";
-  const PLAYERO_WON = "PLAYERO_WON";
-  const TIE = "TIE";
+  /*
+   Indexes within the board
+   [0] [1] [2]
+   [3] [4] [5]
+   [6] [7] [8]
+*/
+
+ 
 });
