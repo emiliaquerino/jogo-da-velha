95b575c0d644301b7839466e9ba3a56e5912f0a8
Emília Querino <emiliaquerino@gmail.com>
2024-02-14 16:34:41 -0300

    Define as constantes de referência html

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  const boxes = Array.from(document.querySelectorAll(".box"));
+  const playerDisplay = document.querySelector(".display-player");
+  const resetButton = document.querySelector("#reset");
+  const announcer = document.querySelector(".announcer");
+
+});
