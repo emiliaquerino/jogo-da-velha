14c89406e6183cd6bdd9b51f151cb2a332cd3ae3
Emília Querino <emiliaquerino@gmail.com>
2024-02-15 17:40:40 -0300

    Altera o nome do jogo

--- script.js
+++ script.js
@@ -52,13 +52,13 @@ window.addEventListener("DOMContentLoaded", () => {
   const announce = (type) => {
     switch (type) {
       case PLAYERO_WON:
-        announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+        announcer.innerHTML = 'Player <span class="playerO">O</span> Won!';
         break;
       case PLAYERX_WON:
-        announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+        announcer.innerHTML = 'Player <span class="playerX">X</span> Won!';
         break;
       case TIE:
-        announcer.innerText = "Tie";
+        announcer.innerText = "TIE!";
     }
     announcer.classList.remove("hide");
   };
@@ -81,14 +81,12 @@ window.addEventListener("DOMContentLoaded", () => {
   
     if (roundWon) {
       announce(currentPlayer === "X" ? PLAYERX_WON : PLAYERO_WON);
-      alert(`Jogador ${currentPlayer} venceu!`);
       isGameActive = false;
       return;
     }
   
     if (!board.includes("")) {
       announce(TIE);
-      alert("Empate!");
     }
   }
   
